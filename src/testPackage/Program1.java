32450e95f82df59a0bf480af16bacb388d8a8bbf
Mosarrof Hossain <phmosharaf@gmail.com>
2018-05-08 00:59:09 -0400

    one is done and ok

--- src/testPackage/Program1.java
+++ src/testPackage/Program1.java
@@ -21,7 +21,7 @@ public class Program1 {
 		// no need any explanation
 		
 System.out.println("This is my first Java Progrm.");
-System.out.println("It is really interesting project");
+System.out.println("It is really interesting and easy project");
 
 System.out.println("You can learn it Step by step .");
 
